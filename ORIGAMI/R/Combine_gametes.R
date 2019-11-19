931865bed547a149537d3c0b711bf44ee74e39c3
JiawenChenn <jwchen1014@gmail.com>
2019-11-19 10:31:58 -0600

    combind_gamete

---
+++ ORIGAMI/R/Combine_gametes.R
@@ -0,0 +1,25 @@
+Combine_gametes<-function(f_id,m_id,chr,cohort,father_path,mother_path,ref_path,out_path){
+  #gemete files
+  f_gamete = data.table::fread(father_path,header = F)
+  m_gamete = data.table::fread(mother_path,header = F)
+
+  #no need to change path
+  allele = data.table::fread(ref_path,header = F)
+  ref = unlist(allele$V4)
+  alt = unlist(allele$V5)
+
+  for(i in 1:ncol(f_gamete)){
+    #replace allele
+    f_gamete[[i]][f_gamete[[i]]==0] = ref[f_gamete[[i]]==0]
+    f_gamete[[i]][f_gamete[[i]]==1] = alt[f_gamete[[i]]==1]
+    m_gamete[[i]][m_gamete[[i]]==0] = ref[m_gamete[[i]]==0]
+    m_gamete[[i]][m_gamete[[i]]==1] = alt[m_gamete[[i]]==1]
+
+    #combine allele
+    temp = paste(f_gamete[[i]],m_gamete[[i]])
+
+    #### have to change substr number
+    famID<-strsplit(f_id,"_")[[1]][1]
+    write(temp,file=out_path,sep = "\t",append=T,ncolumns = length(temp))
+  }
+}
