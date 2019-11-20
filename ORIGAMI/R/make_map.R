52f7f0a781dc6f7dfdebf91403cf6c8fd41bbb16
JiawenChenn <jwchen1014@gmail.com>
2019-11-19 21:19:20 -0600

    ud

--- ORIGAMI/R/make_map.R
+++ ORIGAMI/R/make_map.R
@@ -1,16 +1,16 @@
 ### map BP and rsid together
 make_map = function(cohort,chr,ref_file,rs_file,output_path){
   options(stringsAsFactors = F)
-    data = data.table::fread(rs_file,header=T)
+    data0 = data.table::fread(rs_file,header=T)
     ref = data.table::fread(ref_file,header= F)
 
     #select targeted col for MAP format
-    data = data[,c("CHR","SNP","SNP","BP")]
+    data0 = data0[,c("CHR","SNP","SNP","BP")]
 
     #select chr
-    data = data[which(data[,"CHR"]==as.numeric(chr)),]
+    data0 = data0[which(data0[,"CHR"]==as.numeric(chr)),]
 
     #correct the order
-    data = data[match(ref$V3,data$BP),]
-    data.table::fwrite(data,file=output_path,sep = "\t",row.names=FALSE,col.names=FALSE,quote =F)
+    data0 = data0[match(ref$V3,data$BP),]
+    data.table::fwrite(data0,file=output_path,sep = "\t",row.names=FALSE,col.names=FALSE,quote =F)
 }
