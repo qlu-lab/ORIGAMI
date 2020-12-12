d870f290dbc6e34eafd39367b4ef28008479ae94
JiawenChenn <jwchen1014@gmail.com>
2020-12-12 15:04:41 -0500

    update est file

--- ORIGAMI/R/est.R
+++ ORIGAMI/R/est.R
@@ -12,9 +12,19 @@
 est<-function(cohort,estfile,chr,snp_path,outpath){
   snps=data.table::fread(snp_path,header = F)
   map2=data.table::fread(estfile)
+  snps=data.table::setorder(snps,"V1")
+  if(length(unlist(strsplit(snps$V1[1],"chr")))==2){
+    map2_po = map2$pos[which(map2$pos>=min(snps$V2[which(snps$V1==paste0("chr",chr))]) & map2$pos<=max(snps$V2[which(snps$V1==paste0("chr",chr))]))]
+    map2_cm = map2$cM[which(map2$pos>=min(snps$V2[which(snps$V1==paste0("chr",chr))]) & map2$pos<=max(snps$V2[which(snps$V1==paste0("chr",chr))]))]
+    chr_li = rep(paste0("chr",chr),length(map2_po))
+    map2_newd = data.frame(chr = chr_li,po=map2_po,cm = map2_cm)
+    data.table::fwrite(map2_newd,outpath,quote = F, row.names = F,col.names=F,sep="\t")
+  }else{
   map2_po = map2$pos[which(map2$pos>=min(snps$V2[which(snps$V1==chr)]) & map2$pos<=max(snps$V2[which(snps$V1==chr)]))]
   map2_cm = map2$cM[which(map2$pos>=min(snps$V2[which(snps$V1==chr)]) & map2$pos<=max(snps$V2[which(snps$V1==chr)]))]
   chr_li = rep(chr,length(map2_po))
   map2_newd = data.frame(chr = chr_li,po=map2_po,cm = map2_cm)
   data.table::fwrite(map2_newd,outpath,quote = F, row.names = F,col.names=F,sep="\t")
+  }
 }
+
